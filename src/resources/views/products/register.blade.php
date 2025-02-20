6ed392cd242e709c4c19c5764be3587f0125fcc4
misato kataoka <sakisachi.14@gmail.com>
2025-02-20 11:14:34 +0900

    バリデーションの修正

--- src/resources/views/products/register.blade.php
+++ src/resources/views/products/register.blade.php
@@ -9,37 +9,43 @@
 
     <div class="container">
         <h2 class="title">商品登録</h2>
-        <form action="/products" method="post" enctype="multipart/form-data">
+        <form action="/products/{productId}/update" method="post" enctype="multipart/form-data">
             @csrf
             <div class="form-group">
                 <label for="name">商品名 <span class="required">必須</span></label>
                 <input type="text" id="name" name="name" class="form-control" placeholder="商品名を入力" value="{{ old('name') }}" required />
             </div>
-            <div class="form__error">
-                @error('name')
-                {{ $message }}
-                @enderror
-            </div>
+            @error('name')
+                <span class="input_error">
+                    @foreach ($errors->get('name') as $message)
+                        <p class="input_error_message">{{ $message }}</p>
+                    @endforeach
+                </span>
+            @enderror
 
             <div class="form-group">
                 <label for="price">値段 <span class="required">必須</span></label>
                 <input type="text" id="price" name="price" class="form-control" placeholder="値段を入力" value="{{ old('price') }}" required />
             </div>
-            <div class="form__error">
-                @error('price')
-                {{ $message }}
-                @enderror
-            </div>
+            @error('price')
+                <span class="input_error">
+                    @foreach ($errors->get('price') as $message)
+                        <p class="input_error_message">{{ $message }}</p>
+                    @endforeach
+                </span>
+            @enderror
 
             <div class="form-group">
                 <label for="image">商品画像 <span class="required">必須</span></label>
                 <input type="file" id="image" name="image" class="form-control" required />
             </div>
-            <div class="form__error">
-                @error('image')
-                {{ $message }}
-                @enderror
-            </div>
+            @error('image')
+                <span class="input_error">
+                    @foreach ($errors->get('image') as $message)
+                        <p class="input_error_message">{{ $message }}</p>
+                    @endforeach
+                </span>
+            @enderror
 
             <div class="form-group">
                 <label>季節 <span class="required">必須（複数選択可）</span></label>
@@ -50,21 +56,25 @@
                     <label><input type="checkbox" name="season[]" value="冬"> 冬</label>
                 </div>
             </div>
-            <div class="form__error">
-                @error('season')
-                {{ $message }}
-                @enderror
-            </div>
+            @error('season')
+                <span class="input_error">
+                    @foreach ($errors->get('season') as $message)
+                        <p class="input_error_message">{{ $message }}</p>
+                    @endforeach
+                </span>
+            @enderror
 
             <div class="form-group">
                 <label for="description">商品説明 <span class="required">必須</span></label>
                 <textarea id="description" name="description" class="form-control" rows="4" placeholder="商品の説明を入力" required>{{ old('description') }}</textarea>
             </div>
-            <div class="form__error">
-                @error('description')
-                {{ $message }}
-                @enderror
-            </div>
+            @error('description')
+                <span class="input_error">
+                    @foreach ($errors->get('description') as $message)
+                        <p class="input_error_message">{{ $message }}</p>
+                    @endforeach
+                </span>
+            @enderror
 
             <div class="button-group">
                 <a href="{{ url()->previous() }}" class="btn btn-secondary">戻る</a>
